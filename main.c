d7db166bf712914187607e662ca6484d15fce66b
faye@HomeStation <faye@faye.com>
2018-02-18 01:39:17 +0800

    master branch@HOMESTATION mbstowcs test

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdarg.h>
 
+#include <wchar.h>
+
 int vsn( char *format, ... ){
 	char 	 *cptr;
 	int   	  tmp,     len;
@@ -45,8 +47,50 @@ int vsn( char *format, ... ){
 int main( int argc, char *argv[] ){
 	int tmp;
 	char buf[ 60 ];
+	wchar_t wbuf[ 60 ];
+	char str[]		= "world";
+	wchar_t wstr[]	= L"world";
 	
+	wchar_t wchs[] = L"中";
+
+	wchar_t wchr = 'a';
+
+	wchar_t *wptr;
+
+	tmp = 0;
+	tmp = mbstowcs( NULL, str, 0 );
+	if( tmp > 0 ){
+		tmp++;
+		wptr = NULL;
+		wptr = ( wchar_t* )malloc( sizeof(wchar_t)*tmp );
+		if( wptr == NULL ){
+			perror( "malloc error:" );
+			return 1;
+		}
+	}
+	tmp = mbstowcs( wptr, str, tmp );
+
+	tmp = strlen( str );
+	tmp = wcslen( wstr );
+	tmp = sizeof( buf );
+	tmp = sizeof( str );
+	tmp = sizeof( wchr );
+	tmp = sizeof( wbuf );		
+	tmp = sizeof( wstr );
+	tmp = sizeof( wchs );
+	tmp = swprintf( wbuf, (tmp+1), L"%S\n", wstr );
+	
+	tmp = sizeof( str );
+	tmp = sprintf( buf, "%s\n", str );
+	
+	tmp = sizeof( wchar_t );
+	tmp = sizeof( char );
+
+	tmp = printf( "%S\n", L"KKND" );
+	//printf( "%d\n", tmp );
+
 	memset( buf, 0xff, 60 );
+	tmp = sprintf( buf, "%S\n", L"string is" );
 	tmp = sprintf( buf, "string is: %s\n", "test string" );
 	tmp += sprintf( buf+tmp, "number is: %d\n", 100 );
 	tmp += sprintf( buf+tmp, "character is %c\n", '?' );
